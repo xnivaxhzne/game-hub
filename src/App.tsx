d8d358d37fdcbff45faa5bc272775de8d2004bb0
-g <kavinkumar1543884@gmail.com>
2025-06-19 19:41:26 +0530

    Added game cards

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ function App() {
       <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <GridItem hideBelow="lg" area="aside" bg="gold">
+      <GridItem hideBelow="lg" area="aside">
         Aside
       </GridItem>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         <GameGrid />
       </GridItem>
     </Grid>
