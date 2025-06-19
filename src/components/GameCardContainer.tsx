6ba49d94aabad5c99d67659465a71a78be368d3b
-g <kavinkumar1543884@gmail.com>
2025-06-19 22:18:17 +0530

    Abstract with card container

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react";
+import type { ReactNode } from "react";
+
+interface Prop {
+  children: ReactNode;
+}
+
+const GameCardContainer = ({ children }: Prop) => {
+  return (
+    <Box borderRadius={10} overflow="hidden" width="260px">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card.Root borderRadius={10} overflow="hidden" width="260px">
+    <Card.Root>
       <Skeleton height="200px" />
       <Card.Body>
         <SkeletonText />
