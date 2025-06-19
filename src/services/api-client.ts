8d9e78b0845c0f4103f8bd1ef448476c923c8622
-g <kavinkumar1543884@gmail.com>
2025-06-19 09:58:42 +0530

    Consume RAWG api key from env variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+const apiKey = import.meta.env.VITE_RAWG_API_KEY;
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: ""
+    key: apiKey
   }
 });
