2e4b2828e56e070a025d37166a6c93a4eed8241e
-g <kavinkumar1543884@gmail.com>
2025-06-20 11:46:29 +0530

    Add spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,13 @@
 import useGenres from "@/hooks/useGenres";
 import { getCroppedImageUrl } from "@/services/image-url";
-import { HStack, List, Image, Text } from "@chakra-ui/react";
+import { HStack, List, Image, Text, Spinner } from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+
+  if (isLoading) return <Spinner />;
 
   return (
     <List.Root>
