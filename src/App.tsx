cbaa936aff9fb71444b2d89169ee540727bba14c
-g <kavinkumar1543884@gmail.com>
2025-06-20 12:36:46 +0530

    Add platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { type Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -30,6 +31,7 @@ function App() {
         />
       </GridItem>
       <GridItem area="main">
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,32 @@
+import usePlatforms from "@/hooks/usePlatforms";
+import { Button, Icon, Menu, Portal } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+
+  return (
+    <Menu.Root>
+      <Menu.Trigger asChild>
+        <Button variant="surface">
+          Platform <Icon as={BsChevronDown} />
+        </Button>
+      </Menu.Trigger>
+      <Portal>
+        <Menu.Positioner>
+          <Menu.Content>
+            {data.map((platform) => (
+              <Menu.Item key={platform.id} value={platform.slug}>
+                {platform.name}
+              </Menu.Item>
+            ))}
+          </Menu.Content>
+        </Menu.Positioner>
+      </Portal>
+    </Menu.Root>
+  );
+};
+
+export default PlatformSelector;
