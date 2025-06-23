59ed756852f808e864e0740d324b5c594c36404d
Kavinkumar <kavinkumar1543884@gmail.com>
2025-06-23 09:22:16 +0530

    Add tanstack basic query setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
