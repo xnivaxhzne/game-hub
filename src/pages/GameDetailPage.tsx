2ce47083277cdcf686d27b6099176274da958c82
-g <kavinkumar1543884@gmail.com>
2025-06-25 18:55:26 +0530

    Add game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,5 @@
 import ExpandableText from "@/components/ExpandableText";
+import GameAttributes from "@/components/GameAttributes";
 import useGame from "@/hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router";
@@ -14,6 +15,7 @@ const GameDetailPage = () => {
     <>
       <Heading marginBottom={5}>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
